56544513feaf5ce7e5e5d97be704a46aa7c9ecc1
Bryan Rayner <bryan@rideshark.com>
2016-11-08 17:32:36 -0600

    Update assembly

--- src/NPoco/Properties/AssemblyInfo.cs
+++ src/NPoco/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NPoco")]
+[assembly: AssemblyTitle("RsLibs.NPoco")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e7b1ec16-6205-4dfa-872a-c74674dc37c1")]
+[assembly: Guid("e7b1ec16-6205-4dfa-872a-c74674dc37c4")]
 
 // Version information for an assembly consists of the following four values:
 //
